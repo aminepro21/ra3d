a9461bae9b5e38f3120fe19aa488828cd4ff4cf9
aminepro21 <67024642+aminepro21@users.noreply.github.com>
2021-08-20 00:37:07 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,12 +1,3 @@
-const express = require("express");
-const app = express();
-
-app.listen(() => console.log("Server started"));
-
-app.use('/ping', (req, res) => {
-  res.send(new Date());
-});
-
 const Discord = require('discord.js');
 
 
